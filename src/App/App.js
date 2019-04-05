bd5c5e0d6f9ae820f1321284bbbed783e729cde0
MenSeb <sebastien.menard.github@gmail.com>
2019-04-05 03:24:24 -0400

    update App structure, testing Galaxy with System

--- src/App/App.js
+++ src/App/App.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import { System, Galaxy } from '../components';
+import { SYSTEM } from '../constants';
 
 export default function App ()
 {
   return (
     <div className='App'>
+
+      <Galaxy color={ '555555' } elements={ SYSTEM }>
+
+        <System elements={ SYSTEM } />
+
+      </Galaxy>
+
     </div>
   )
 }
