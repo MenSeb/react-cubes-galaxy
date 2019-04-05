2e70abaac4bdf712ab5cc672b7f901f72b07392d
MenSeb <sebastien.menard.github@gmail.com>
2019-04-05 19:09:05 -0400

    using random values/functions for tests

--- src/constants/system.js
+++ src/constants/system.js
@@ -1,29 +1,46 @@
 import uid from 'uniqid';
 
-const orbitColors = [ '333333', '888888', 'BBBBBB' ]
+const orbitColor = '38598b'
+const rotation = { x: 1, y: 1, z: 1, t: 360 }
+const hex = '0123456789ABCDEF'
+const starSize = 100
+
+const randomHex = () => hex[ Math.floor( Math.random() * hex.length ) ]
+const randomHexColor = () => Array.from( { length: 3 }, _ => randomHex() ).join( '' )
+const randomTime = () => Math.random() * 10 + 5
+
+const planets = Array.from(
+  { length: 8 },
+  ( _, index ) =>
+  {
+    const planetSize = Math.random() * 25 + 25
+
+    return {
+      distance: ( index + 1 ) * 50 + starSize,
+      star: {
+        color: randomHexColor(),
+        size: planetSize,
+        time: randomTime(),
+        rotation
+      },
+      orbit: {
+        color: orbitColor,
+        size: planetSize,
+        time: randomTime()
+      }
+    }
+  }
+)
 
 export default [
   {
     distance: 0,
     star: {
-      color: 'FFFF00',
-      rotation: { x: 1, y: 1, z: 1, t: 360 },
-      size: 100,
-      time: 10,
+      color: randomHexColor(),
+      rotation,
+      size: starSize,
+      time: randomTime(),
     }
   },
-  {
-    distance: 150,
-    star: {
-      color: 'FF0000',
-      rotation: { y: 1, t: 360 },
-      size: 20,
-      time: 6,
-    },
-    orbit: {
-      colors: orbitColors,
-      size: 2,
-      time: 4
-    }
-  }
+  ...planets
 ].map( element => ({ id: uid(), ...element }) )
