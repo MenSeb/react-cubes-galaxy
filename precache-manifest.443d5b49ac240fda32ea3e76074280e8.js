2b8bef81c8b89e5ed0ac6afa6d4c0facd5d12f7b
MenSeb <sebastien.menard.github@gmail.com>
2019-04-05 19:19:52 -0400

    Updates

---
+++ precache-manifest.443d5b49ac240fda32ea3e76074280e8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a60b67aacf8b910367ad",
+    "url": "/react-cubes-galaxy/static/css/main.acbc27dc.chunk.css"
+  },
+  {
+    "revision": "a60b67aacf8b910367ad",
+    "url": "/react-cubes-galaxy/static/js/main.4043424d.chunk.js"
+  },
+  {
+    "revision": "0b75590308d099978031",
+    "url": "/react-cubes-galaxy/static/js/runtime~main.23e8d1b7.js"
+  },
+  {
+    "revision": "ba5a5181776aaf594050",
+    "url": "/react-cubes-galaxy/static/js/2.a5d73e58.chunk.js"
+  },
+  {
+    "revision": "93b09422bf03bb507034cb80672a120e",
+    "url": "/react-cubes-galaxy/index.html"
+  }
+];
